b81c2d18dc4a9bc40abd84af1453441e3c4655a7
Hugo Baraúna <hugo.barauna@gmail.com>
2022-11-22 16:25:12 -0300

    First working prototype

--- lib/kino_slack.ex
+++ lib/kino_slack.ex
@@ -1,18 +1,76 @@
 defmodule KinoSlack do
-  @moduledoc """
-  Documentation for `KinoSlack`.
-  """
+  use Kino.JS, assets_path: "lib/assets"
+  use Kino.JS.Live
+  use Kino.SmartCell, name: "Slack"
 
-  @doc """
-  Hello world.
+  @impl true
+  def init(attrs, ctx) do
+    fields = %{
+      "token" => attrs["fields"]["token"] || "",
+      "channel" => attrs["fields"]["channel"] || "",
+      "message" => attrs["fields"]["message"] || ""
+    }
 
-  ## Examples
+    ctx = assign(ctx, fields: fields)
+    {:ok, ctx}
+  end
 
-      iex> KinoSlack.hello()
-      :world
+  @impl true
+  def handle_connect(ctx) do
+    {:ok, %{fields: ctx.assigns.fields}, ctx}
+  end
 
-  """
-  def hello do
-    :world
+  @impl true
+  def handle_event("update_token", value, ctx) do
+    ctx = update(ctx, :fields, &Map.merge(&1, %{"token" => value}))
+    {:noreply, ctx}
+  end
+
+  @impl true
+  def handle_event("update_channel", value, ctx) do
+    ctx = update(ctx, :fields, &Map.merge(&1, %{"channel" => value}))
+    {:noreply, ctx}
+  end
+
+  @impl true
+  def handle_event("update_message", value, ctx) do
+    ctx = update(ctx, :fields, &Map.merge(&1, %{"message" => value}))
+    {:noreply, ctx}
+  end
+
+  @impl true
+  def to_attrs(ctx) do
+    %{
+      "fields" => ctx.assigns.fields
+    }
+  end
+
+  @impl true
+  def to_source(attrs) do
+    if any_field_empty?(attrs) do
+      ""
+    else
+      quote do
+        req =
+          Req.new(
+            base_url: "https://slack.com/api",
+            auth: {:bearer, unquote(attrs["fields"]["token"])}
+          )
+
+        Req.post!(req,
+          url: "/chat.postMessage",
+          json: %{
+            channel: unquote(attrs["fields"]["channel"]),
+            text: unquote(attrs["fields"]["message"])
+          }
+        )
+      end
+      |> Kino.SmartCell.quoted_to_string()
+    end
+  end
+
+  def any_field_empty?(attrs) do
+    keys = Map.keys(attrs["fields"])
+    Enum.any?(keys, fn key -> attrs["fields"][key] in [nil, ""] end)
   end
 end
--- test/kino_slack_test.exs
+++ test/kino_slack_test.exs
@@ -1,8 +1,43 @@
 defmodule KinoSlackTest do
   use ExUnit.Case
-  doctest KinoSlack
 
-  test "greets the world" do
-    assert KinoSlack.hello() == :world
+  import Kino.Test
+
+  setup :configure_livebook_bridge
+
+  describe "code generation" do
+    test "returns correct code when all fields are filled" do
+      attrs = %{
+        "fields" => %{
+          "token" => "xoxb-slack-token",
+          "channel" => "slack-channel",
+          "message" => "text message"
+        }
+      }
+
+      {_kino, source} = start_smart_cell!(KinoSlack, attrs)
+
+      assert source == """
+             req = Req.new(base_url: "https://slack.com/api", auth: {:bearer, "xoxb-slack-token"})
+
+             Req.post!(req,
+               url: "/chat.postMessage",
+               json: %{channel: "slack-channel", text: "text message"}
+             )\
+             """
+    end
+
+    test "returns empty source code when any of the fields is empty" do
+      attrs = %{
+        "fields" => %{
+          "token" => "xoxb-slack-token",
+          "channel" => "slack-channel",
+          "message" => "text message"
+        }
+      }
+
+      attrs = put_in(attrs["fields"]["token"], "")
+      assert KinoSlack.to_source(attrs) == ""
+    end
   end
 end
