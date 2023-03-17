bf443f1f66ab8289661c0266f9ae0e25b638b2bd
Hugo Baraúna <hugo.barauna@gmail.com>
2023-03-17 10:49:53 -0300

    Avoids appending to the end of a list

--- lib/kino_slack/message_interpolator.ex
+++ lib/kino_slack/message_interpolator.ex
@@ -2,7 +2,8 @@ defmodule KinoSlack.MessageInterpolator do
   @moduledoc false
 
   def interpolate(message) do
-    args = build_interpolation_args(message, [])
+    args = build_interpolation_args(message, [""])
+    args = Enum.reverse(args)
     {:<<>>, [], args}
   end
 
@@ -13,21 +14,21 @@ defmodule KinoSlack.MessageInterpolator do
   defp build_interpolation_args("{{" <> rest, args) do
     with [inner, rest] <- String.split(rest, "}}", parts: 2),
          {:ok, expression} <- Code.string_to_quoted(inner) do
-      args = append_interpolation(args, expression)
+      args = add_interpolation(args, expression)
       build_interpolation_args(rest, args)
     else
       _ ->
-        args = append_string(args, "{{")
+        args = add_string(args, "{{")
         build_interpolation_args(rest, args)
     end
   end
 
   defp build_interpolation_args(<<char::utf8, rest::binary>>, args) do
-    args = append_string(args, <<char::utf8>>)
+    args = add_string(args, <<char::utf8>>)
     build_interpolation_args(rest, args)
   end
 
-  defp append_interpolation(args, expression) do
+  defp add_interpolation(args, expression) do
     interpolation_node = {
       :"::",
       [],
@@ -37,17 +38,16 @@ defmodule KinoSlack.MessageInterpolator do
       ]
     }
 
-    args ++ [interpolation_node]
+    [interpolation_node | args]
   end
 
-  defp append_string(args, string) do
-    last_arg = List.last(args)
+  defp add_string(args, string) do
+    [head | tail] = args
 
-    if is_binary(last_arg) do
-      last_string = last_arg <> string
-      List.replace_at(args, -1, last_string)
+    if is_binary(head) do
+      [head <> string | tail]
     else
-      args ++ [string]
+      [string | args]
     end
   end
 end
